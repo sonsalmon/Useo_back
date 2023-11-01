13b7f1d1b7fa277215b58579ee42fa377c01de98
sonsalmon <sjmjis0208@gmail.com>
2023-11-01 13:24:45 +0900

    [REFACTOR] url 수정

--- notes/urls.py
+++ notes/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from .views import NoteListCreateView, NoteRetrieveUpdateDestroyView
 
 urlpatterns = [
-    path('create/list/', NoteListCreateView.as_view(), name='note-list-create'),
-    path('get_list/',NoteListCreateView.as_view()),
+    path('list/', NoteListCreateView.as_view(), name='note-list-create-or-get'),
     path('note/<int:pk>/', NoteRetrieveUpdateDestroyView.as_view()),
     # path('patch/<int:pk>',NoteRetrieveUpdateDestroyView.as_view()),
     # path('delete/<int:pk>',NoteRetrieveUpdateDestroyView.as_view()),
